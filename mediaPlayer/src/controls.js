189118e7955c096fc37b83238f41a21caf7249d7
muniek.k@gmail.com <muniek.k@gmail.com>
2020-09-07 17:13:18 -0400

    refactoring control.js

--- mediaPlayer/src/controls.js
+++ mediaPlayer/src/controls.js
@@ -1,143 +1,45 @@
-var player = document.querySelector("video");
 
-$('#progress01').click(function(e) {
-  let currPosi = e.offsetX / this.offsetWidth * 100;
-  player.currentTime = currPosi/100 * player.duration;
-  updateProgressBar(currPosi, "progress-bar01");
-  clearPBars(2);
-});
+var ul = document.getElementById('playerPB');
+ul.onclick = function(event) {
 
-$('#progress02').click(function(e) {
-  let currPosi = e.offsetX / this.offsetWidth * 100;
-  updateProgressBar(currPosi, "progress-bar02");
-  fillPBars(1);
-  clearPBars(3);
-});
+  let target = getEventTarget(event);
+  let currPID = target.id; 
+  let currPNB = currPID.slice(-2);
 
-$('#progress03').click(function(e) {
-  let currPosi = e.offsetX / this.offsetWidth * 100;
-  updateProgressBar(currPosi, "progress-bar03");
-  fillPBars(2);
-  clearPBars(4);
-});
-$('#progress04').click(function(e) {
-  let currPosi = e.offsetX / this.offsetWidth * 100;
-  updateProgressBar(currPosi, "progress-bar04");
-  fillPBars(3);
-  clearPBars(5);
-});
-$('#progress05').click(function(e) {
-  let currPosi = e.offsetX / this.offsetWidth * 100;
-  updateProgressBar(currPosi, "progress-bar05");
-  fillPBars(4);
-  clearPBars(6);
-});
-$('#progress06').click(function(e) {
-  let currPosi = e.offsetX / this.offsetWidth * 100;
-  updateProgressBar(currPosi, "progress-bar06");
-  fillPBars(5);
-  clearPBars(7);
-});
-$('#progress07').click(function(e) {
-  let currPosi = e.offsetX / this.offsetWidth * 100;
-  updateProgressBar(currPosi, "progress-bar07");
-  fillPBars(6);
-  clearPBars(8);
-});
-$('#progress08').click(function(e) {
-  let currPosi = e.offsetX / this.offsetWidth * 100;
-  updateProgressBar(currPosi, "progress-bar08");
-  fillPBars(7);
-  clearPBars(9);
-});
-$('#progress09').click(function(e) {
-  let currPosi = e.offsetX / this.offsetWidth * 100;
-  updateProgressBar(currPosi, "progress-bar09");
-  fillPBars(8);
-  clearPBars(10);
-});
-$('#progress10').click(function(e) {
-  let currPosi = e.offsetX / this.offsetWidth * 100;
-  updateProgressBar(currPosi, "progress-bar10");
-  fillPBars(9);
-  clearPBars(11);
-});
-$('#progress11').click(function(e) {
-  let currPosi = e.offsetX / this.offsetWidth * 100;
-  updateProgressBar(currPosi, "progress-bar11");
-  fillPBars(10);
-  clearPBars(12);
-});
-$('#progress12').click(function(e) {
-  let currPosi = e.offsetX / this.offsetWidth * 100;
-  updateProgressBar(currPosi, "progress-bar12");
-  fillPBars(11);
-  clearPBars(13);
-});
-$('#progress13').click(function(e) {
-  let currPosi = e.offsetX / this.offsetWidth * 100;
-  updateProgressBar(currPosi, "progress-bar13");
-  fillPBars(12);
-  clearPBars(14);
-});
-$('#progress14').click(function(e) {
-  let currPosi = e.offsetX / this.offsetWidth * 100;
-  updateProgressBar(currPosi, "progress-bar14");
-  fillPBars(13);
-  clearPBars(15);
-});
-$('#progress15').click(function(e) {
-  let currPosi = e.offsetX / this.offsetWidth * 100;
-  updateProgressBar(currPosi, "progress-bar15");
-  fillPBars(14);
-});
+  //order is important
+  let currPBID = `progress-bar${currPNB}`; 
+  (currPNB < 10) ? currPNB = currPNB.slice(-1): '';
+  
+  let currPosi = event.offsetX / this.offsetWidth * 100;
+  updateProgressBar(currPosi, currPBID);
+  
+  fillPBars(parseInt(currPNB)-1);
+  clearPBars(parseInt(currPNB)+1);
+};
 
-function fillPBars(maxPB){
-  for (let i = 1; i <= maxPB; i++){
+function getEventTarget(e) {
+  e = e || window.event;
+  return e.target || e.srcElement; 
+}
+
+function fillPBars(maxPBToFil){
+  for (let i = 1; i <= maxPBToFil; i++){
     let currPB = ''; 
     (i<10) ? currPB = `progress-bar0${i}`: currPB = `progress-bar${i}`;
     updateProgressBar(100, currPB);
   }
 }
-export function clearPBars(minPB){
-  for (let i = minPB; i <= 15; i++){
-    let currPB = 'progress-bar15'; 
+export function clearPBars(minPBToClear){
+  for (let i = minPBToClear; i <= 15; i++){
+    let currPB = ''; 
     (i<10) ? currPB = `progress-bar0${i}`: currPB = `progress-bar${i}`;
     updateProgressBar(0, currPB);
   }
 }
+
 function updateProgressBar(value, progressBar){
   var pb = document.getElementById(progressBar);
   pb.style.width = `${value}%`;
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function getEventTarget(e) {
-//   e = e || window.event;
-//   return e.target || e.srcElement; 
-// }
-
-// var ul = document.getElementById('controls');
-// ul.onclick = function(event) {
-//   var target = getEventTarget(event);
-//   console.log(target);
-// };
